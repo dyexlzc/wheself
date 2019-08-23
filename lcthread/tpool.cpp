90a6b981d53c1e653ce27ea8218ed79eeb9bdebf
dyexlzc <38608187+dyexlzc@users.noreply.github.com>
2019-08-23 07:18:14 +0000

    Update tpool.cpp

--- lcthread/tpool.cpp
+++ lcthread/tpool.cpp
@@ -10,6 +10,7 @@
 #include<queue>
 #include<condition_variable>
 #include<string>
+#include<iostream>
 using namespace std;
 class ThreadPool {
 public:
@@ -25,11 +26,15 @@ private:
 	int num_running, n;//任务个数
 public:
 
-	ThreadPool(int const &_n) :n(_n) {
+	ThreadPool(int const &_n=0):n(_n) {
 		//初始化线程池个数
+		if (_n == 0) {
+			this->n = std::move(this->core_num());
+		}
+		cout << this->n;
 		num_running = 0;
-		tlist = new thread[_n];
-		for (int i = 0; i < _n; i++) {
+		tlist = new thread[n];
+		for (int i = 0; i < n; i++) {
 			tlist[i] = thread([this, i]() {//直接在这里用lambda表达式建立线程
 
 				while (1) {
@@ -53,39 +58,7 @@ public:
 
 		}
 	}
-	ThreadPool() {
-		int _n = this->core_num();
-		this->n = _n;
-		//初始化线程池个数
-		num_running = 0;
-		tlist = new thread[_n];
-		for (int i = 0; i < _n; i++) {
-			tlist[i] = thread([this, i]() {//直接在这里用lambda表达式建立线程
 
-				while (1) {
-					unique_lock<mutex> lock(this->job_mutex);
-					this->cv.wait(lock, [this] {
-						return !this->_job.empty();
-					});//等待阻塞线程，直到update中通知状态改变
-					if (this->_job.size() != 0)
-					{
-						this->mutex_global.lock();
-						job* job = this->_job.front(); this->_job.pop();
-						lock.unlock();
-						this->num_running++;
-						this->mutex_global.unlock();
-
-						job->run();
-
-						this->mutex_global.lock();
-						this->num_running--;
-						this->mutex_global.unlock();
-					}
-				}
-			});
-
-		}
-	}
 	void addTask(job& job) {
 		this->_job.push(&job);
 		this->update();
